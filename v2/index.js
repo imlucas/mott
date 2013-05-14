c06d2f36c0fe7a8cbaef02b3a7e88d89e98371f5
Lucas Hrabovsky <hrabovsky.lucas@gmail.com>
2013-05-14 11:27:28 -0400

    Ensure build dir and copy includes in default recipe

--- v2/index.js
+++ v2/index.js
@@ -1,14 +1,51 @@
 "use strict";
 
-var Recipe = require('./recipe');
+var Recipe = require('./recipe'),
+    fs = require('fs'),
+    Glob = require('glob').Glob,
+    Q = require('q');
 
 module.exports = function(name){
     return new Recipe(name)
         .register('make build dir', function(ctx, done){
-            done();
+            fs.exists('build', function(exists){
+                if(exists){
+                    return done();
+                }
+                fs.mkdir('build', done);
+            });
         })
         .register('copy includes', function(ctx, done){
-            done();
+            if(!ctx.includes || ctx.includes.length === 0){
+                return done();
+            }
+            var seen = [];
+            Q.all(ctx.includes.map(function(include){
+                var d = Q.defer();
+                new Glob(include, {match: true}, function(matches){
+                    matches.map(function(match){
+                        if(seen.indexOf(match) === -1){
+                            seen.push(match);
+                        }
+                    });
+                    d.resolve();
+                });
+                return d.promise;
+            })).then(function(){
+                return Q.all(seen.map(function(src){
+                    var d = Q.defer(),
+                        readStream = fs.createReadStream(src),
+                        writeStream = fs.createWriteStream('build/' + src);
+
+                    readStream.pipe(writeStream);
+                    readStream.once('end', function(){
+                        d.resolve();
+                    });
+                    return d.promise;
+                }));
+            }).then(function(){
+                done();
+            }).done();
         })
         .task('build', ['make build dir', 'copy includes']);
 };
