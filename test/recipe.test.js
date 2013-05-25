b1d726f3603f90ce145dfa23fff9c0a5d845e99d
Lucas Hrabovsky <hrabovsky.lucas@gmail.com>
2013-05-25 15:38:29 -0400

    Test for registering non array steps for a task

--- test/recipe.test.js
+++ test/recipe.test.js
@@ -4,14 +4,24 @@ var assert = require('assert');
 
 var Recipe = require('../lib/recipe');
 
+var emptyStep = function(ctx, cb){
+    cb();
+};
+
 describe("Recipe", function(){
     it("should register a step", function(){
-        var func = function(ctx, cb){
-                cb();
-            },
-            r = new Recipe()
-                .step('t', func);
+        var r = new Recipe()
+                .step('t', emptyStep);
 
-        assert.deepEqual(r.steps, {'t': [func]});
+        assert.deepEqual(r.steps, {'t': [emptyStep]});
+    });
+
+    it("should register a task", function(){
+        var r = new Recipe()
+            .step('t', emptyStep)
+            .task('empty', 't');
+
+
+        assert.deepEqual(r.tasks, {"empty":[{"steps":["t"],"mode":"sequential"}]});
     });
 });
