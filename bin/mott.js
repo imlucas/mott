d19327d32177310ce1bdd1787f6ff62298916cc1
Lucas Hrabovsky <hrabovsky.lucas@gmail.com>
2013-06-04 11:07:38 -0400

    Working.  Still need to think combine config and environments everywhere.

--- bin/mott.js
+++ bin/mott.js
@@ -1,9 +1,39 @@
 #!/usr/bin/env node
 
+"use strict";
+
 var mott = require('../'),
     fs = require('fs-extra');
 
-// mott.quickstart();
+// @todo (lucas) If a local mott exists, shell to that bin
 
-// Parse package
-fs.readJson('./package.json', console.log);
+fs.readJson('./package.json', function(err, data){
+    if(err){
+        console.log(err);
+        return;
+    }
+    var environments = {},
+        metadata,
+        key,
+        recipe;
+
+    for(key in data.mott){
+        if(key !== "recipe"){
+            environments[key] = data.mott[key];
+        }
+    }
+
+    recipe = mott();
+    for(key in data.mott.recipe){
+        if(key === "metadata"){
+            metadata = data.mott.recipe.metadata;
+        }
+        else if(key === "use"){
+            recipe.use(mott.resolve(data.mott.recipe[key]));
+        }
+        else {
+            recipe.task(key, mott.resolve(data.mott.recipe[key]));
+        }
+    }
+    recipe.cli(metadata, {}, environments);
+});
